62b7691625cbbc34758dead6fcc8c16971100f4e
vincentzyc <vincentzyc@qq.com>
2023-06-29 11:23:53 +0800

    🎉init: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3100,
+  },
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
