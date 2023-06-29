15ca291e6963c80bc521e819e242d736af287a10
vincentzyc <vincentzyc@qq.com>
2023-06-29 11:24:05 +0800

    🎉init: init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import '@/assets/css/base.css';
+import '@/assets/css/main.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
