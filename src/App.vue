b03ef6bece0d71786dc0998c5ac71ad4024d33bb
vincentzyc <vincentzyc@qq.com>
2023-07-02 01:14:18 +0800

    🎉 init: init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 // import ImgPDF from './components/ImgPDF.vue';
 import CanvasPDF from './components/CanvasPDF.vue';
+// import IframePDF from './components/IframePDF.vue';
 </script>
 
 <template>
   <!-- <ImgPDF /> -->
   <CanvasPDF />
+  <!-- <IframePDF /> -->
 </template>
